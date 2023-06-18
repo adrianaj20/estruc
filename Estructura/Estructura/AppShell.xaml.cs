e9d43907118756d713a2bfc123760073b6e0f711
adrianaj20 <adrian.najarro@tecsup.edu.pe>
2023-06-17 22:36:01 -0500

    first commit

---
+++ Estructura/Estructura/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Estructura.ViewModels;
+using Estructura.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Estructura
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
